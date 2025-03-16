7df2508ad183e361cab39ae1fbf6469177aad48d
Nabil164 <mhmdnabil235@gmail.com>
2025-03-16 17:07:39 +0700

    Menambahkan Controller dan View untuk PBL

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+    <h2>Halaman Dashboard</h2>
+</body>
+</html>
---
+++ resources/views/list_item.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>List Item</title>
+</head>
+<body>
+    <h2>Daftar Item</h2>
+</body>
+</html>
