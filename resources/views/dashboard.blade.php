898e71359d74f1803866acf20cc0e78db161cce5
Nabil164 <mhmdnabil235@gmail.com>
2025-11-19 19:11:07 +0700

    Delete resources directory

--- resources/views/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Dashboard</title>
-</head>
-<body>
-    <h2>Halaman Dashboard</h2>
-</body>
-</html>
--- resources/views/list_item.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>List Item</title>
-</head>
-<body>
-    <h2>Daftar Item</h2>
-</body>
-</html>
